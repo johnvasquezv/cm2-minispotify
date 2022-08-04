2203df6e1f932a11b410662a60fbf9cba2108b2a
John Vasquez <johnv@synergyecuador.com>
2022-08-04 16:59:47 -0500

    Definición del controlador Artista con todos sus metodos CRUD

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = express();
 //cargar rutas
 
 var user_routes = require('./routes/user');
+var artist_routes = require('./routes/artist');
 
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -19,5 +20,6 @@ app.use(bodyParser.json());
 //rutas base
 
 app.use('/api', user_routes);
+app.use('/api', artist_routes);
 
 module.exports = app;
